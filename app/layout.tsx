fbd5c760f9b8131a3ff278e4f8a04f4fc48a6848
tomoaki-nozaki <t-nozaki@delta-x.co.jp>
2026-06-27 02:21:22 +0900

    feat: add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -123,6 +124,7 @@ export default function RootLayout({
             </p>
           </div>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
